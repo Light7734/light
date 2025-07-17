4ff69dea7c103f70cff6cff01d9c664d5936a235
light7734 <light7734@tuta.io>
2025-07-17 12:17:59 +0330

    tools: add some logs to build_ci_images script

--- tools/scripts/build_ci_images.sh
+++ tools/scripts/build_ci_images.sh
@@ -4,15 +4,19 @@ set -e
 
 IMAGE_DIR="$(git rev-parse --show-toplevel)/tools/ci/images"
 
+echo "==> Building image: clang format"
 cd "$IMAGE_DIR/clang_format"
 docker build -t clang_format .
 
+echo "==> Building image: static analysis"
 cd "$IMAGE_DIR/static_analysis"
 docker build -t static_analysis .
 
+echo "==> Building image: unit tests"
 cd "$IMAGE_DIR/unit_tests"
 docker build -t unit_tests .
 
+echo "==> Building image: valgrind"
 cd "$IMAGE_DIR/valgrind"
 docker build -t valgrind .
 
