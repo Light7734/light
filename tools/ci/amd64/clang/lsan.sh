5c96e2deb9c933c8e93902649ef8ac6315abbe51
light7734 <light7734@tuta.io>
2025-09-21 13:49:03 +0330

    ci(amd64/clang/lsan): replace conan commands with cmake

--- tools/ci/amd64/clang/lsan.sh
+++ tools/ci/amd64/clang/lsan.sh
@@ -7,20 +7,25 @@ rm -rf ./build
 Xvfb :99 -screen 0 1024x768x16 &
 export DISPLAY=:99
 
-conan build . \
-  -c tools.system.package_manager:mode=install \
-  -c tools.cmake.cmaketoolchain:generator=Ninja \
-  -c tools.build:cxxflags='["-g", "-fno-omit-frame-pointer", "-nostdinc++", "-isystem", "/libcxx_lsan/include/c++/v1/", "-fsanitize=leak"]' \
-  -c tools.build:sharedlinkflags='["-L/libcxx_lsan/lib", "-Wl,-rpath,/libcxx_lsan/lib", "-lc++", "-lc++abi", "-fsanitize=leak"]' \
-  -c tools.build:exelinkflags='["-L/libcxx_lsan/lib", "-Wl,-rpath,/libcxx_lsan/lib", "-lc++", "-lc++abi", "-fsanitize=leak"]' \
-  -c tools.info.package_id:confs='["tools.build:cxxflags","tools.build:sharedlinkflags","tools.build:exelinkflags"]' \
-  -c tools.build:compiler_executables='{"c": "clang", "cpp": "clang++"}' \
-  -s build_type=Release \
-  -s compiler=clang \
-  -s compiler.version=20 \
-  -s compiler.libcxx=libc++ \
-  -o use_mold=True \
-  --build=missing
+cmake .. \
+-G Ninja \
+-DCMAKE_LINKER_TYPE=MOLD \
+-DENABLE_UNIT_TESTS=ON \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_CXX_FLAGS="
+-fsanitize=leak
+-g
+-fno-omit-frame-pointer
+-std=c++23
+-nostdinc++
+-isystem /libcxx_lsan/include/c++/v1/" \
+-DCMAKE_EXE_LINKER_FLAGS="
+-fsanitize=leak
+-L/libcxx_lsan/lib
+-lc++
+-lc++abi
+-Wl,-rpath,/libcxx_lsan/lib" \
+&& cmake --build . -j`nproc`
 
 for test in $(find ./build -type f -name '*_tests' -executable); do
   echo "Running $test"
