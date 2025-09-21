3d3ddd2197f89c8d534cce9ae56d39f4be8e4eb9
light7734 <light7734@tuta.io>
2025-09-21 13:54:57 +0330

    ci(amd64/clang/msan): replace conan commands with cmake

--- tools/ci/amd64/clang/msan.sh
+++ tools/ci/amd64/clang/msan.sh
@@ -5,22 +5,31 @@ cd $(git rev-parse --show-toplevel)/
 rm -rf ./build
 
 Xvfb :99 -screen 0 1024x768x16 &
+export CXX=$(which clang++)
+export CC=$(which clang)
 export DISPLAY=:99
 
-conan build . \
-    -c tools.system.package_manager:mode=install \
-    -c tools.cmake.cmaketoolchain:generator=Ninja \
-    -c tools.build:cxxflags='["-g", "-fno-omit-frame-pointer", "-nostdinc++", "-isystem", "/libcxx_msan/include/c++/v1/", "-fsanitize=memory", "-fsanitize-memory-track-origins"]' \
-    -c tools.build:sharedlinkflags='["-L/libcxx_msan/lib", "-Wl,-rpath,/libcxx_msan/lib", "-lc++", "-lc++abi", "-fsanitize=memory", "-fsanitize-memory-track-origins"]' \
-    -c tools.build:exelinkflags='["-L/libcxx_msan/lib", "-Wl,-rpath,/libcxx_msan/lib", "-lc++", "-lc++abi", "-fsanitize=memory", "-fsanitize-memory-track-origins"]' \
-    -c tools.info.package_id:confs='["tools.build:cxxflags","tools.build:sharedlinkflags","tools.build:exelinkflags"]' \
-    -c tools.build:compiler_executables='{"c": "clang", "cpp": "clang++"}' \
-    -s build_type=Release \
-    -s compiler=clang \
-    -s compiler.version=20 \
-    -s compiler.libcxx=libc++ \
-    -o use_mold=True \
-    --build=missing
+cmake .. \
+-G Ninja \
+-DCMAKE_LINKER_TYPE=MOLD \
+-DENABLE_UNIT_TESTS=ON \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_CXX_FLAGS=" \
+-fsanitize=memory \
+-fsanitize-memory-track-origins \
+-g \
+-fno-omit-frame-pointer \
+-std=c++23 \
+-nostdinc++ \
+-isystem /libcxx_msan/include/c++/v1/" \
+-DCMAKE_EXE_LINKER_FLAGS=" \
+-fsanitize=memory \
+-fsanitize-memory-track-origins \
+-L/libcxx_msan/lib \
+-lc++ \
+-lc++abi \
+-Wl,-rpath,/libcxx_msan/lib" \
+&& cmake --build . -j`nproc`
 
 for test in $(find ./build -type f -name '*_tests' -executable); do
   echo "Running $test"
