5de1037e9390ebc0cbe001970571bb11dc53e33c
light7734 <light7734@tuta.io>
2025-09-21 13:52:32 +0330

    ci(amd64/clang/lsan): fix

--- tools/ci/amd64/clang/lsan.sh
+++ tools/ci/amd64/clang/lsan.sh
@@ -3,27 +3,29 @@
 set -e
 cd $(git rev-parse --show-toplevel)/
 rm -rf ./build
-
 Xvfb :99 -screen 0 1024x768x16 &
+
 export DISPLAY=:99
+export CXX=$(which clang++)
+export CC=$(which clang)
 
 cmake .. \
 -G Ninja \
 -DCMAKE_LINKER_TYPE=MOLD \
 -DENABLE_UNIT_TESTS=ON \
 -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_CXX_FLAGS="
--fsanitize=leak
--g
--fno-omit-frame-pointer
--std=c++23
--nostdinc++
+-DCMAKE_CXX_FLAGS=" \
+-fsanitize=leak \
+-g \
+-fno-omit-frame-pointer \
+-std=c++23 \
+-nostdinc++ \
 -isystem /libcxx_lsan/include/c++/v1/" \
--DCMAKE_EXE_LINKER_FLAGS="
--fsanitize=leak
--L/libcxx_lsan/lib
--lc++
--lc++abi
+-DCMAKE_EXE_LINKER_FLAGS=" \
+-fsanitize=leak \
+-L/libcxx_lsan/lib \
+-lc++ \
+-lc++abi \
 -Wl,-rpath,/libcxx_lsan/lib" \
 && cmake --build . -j`nproc`
 
