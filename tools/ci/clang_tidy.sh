90773aa42d7b7919b0b4b8d7490d7fefcf04dcd9
light7734 <light7734@tuta.io>
2026-08-30 16:19:29 +0330

    ci/clang_tidy: enable -Werror

--- tools/ci/clang_tidy.sh
+++ tools/ci/clang_tidy.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 cd "$(git rev-parse --show-toplevel)/"
 rm -rf ./build/
 
+# We need the `-Wno-reserved-module-identifier` because clang generates a warning when doing the std module...
+# TODO(Light): resolve warning from std module somehow and remove `-Wno-reserved-module-identifier`
 cmake \
     -S . \
     -B build \
@@ -11,7 +13,7 @@ cmake \
     -D CMAKE_LINKER_TYPE=MOLD \
     -D CMAKE_C_COMPILER="$(which clang)" \
     -D CMAKE_CXX_COMPILER="$(which clang++)" \
-    -D CMAKE_CXX_FLAGS="-std=c++26 -stdlib=libc++" \
+    -D CMAKE_CXX_FLAGS="-std=c++26 -stdlib=libc++ -Werror -Wno-reserved-module-identifier" \
     -D CMAKE_EXPORT_COMPILE_COMMANDS=TRUE \
     -D ENABLE_UNIT_TESTS=ON \
     -D ENABLE_STATIC_ANALYSIS=ON \
