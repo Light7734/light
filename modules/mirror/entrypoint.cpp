d47cdc4db96444887134f85c8853f70eaacbfd0a
light7734 <light7734@tuta.io>
2026-09-02 21:17:29 +0330

    build: add entrypoint.cpp to mirror and modified the old file

--- modules/mirror/entrypoint.cpp
+++ modules/mirror/entrypoint.cpp
@@ -1,32 +1,28 @@
+import preliminary;
+
 import app;
 import app.system;
 import std;
 import logger;
 import mirror.system;
-import renderer.factory;
 
-/** The ultimate entrypoint. */
 auto main(i32 argc, char *argv[]) -> i32
+try
 {
-	try
+	ignore = argc;
+	ignore = argv;
+
+	auto application = create_scope<lt::Mirror>();
+	if (!application)
 	{
-		ignore = argc;
-		ignore = argv;
-
-		auto application = create_scope<lt::Mirror>();
-		if (!application)
-		{
-			throw std::runtime_error { "Failed to create application\n" };
-		}
-
-		application->game_loop();
-		return 0;
+		throw std::runtime_error { "Failed to create application\n" };
 	}
-	catch (const std::exception &exp)
-	{
-		lt::log::critical("Terminating due to uncaught exception:");
-		lt::log::critical("\texception.what(): {}", exp.what());
 
-		return 1;
-	}
+	application->game_loop();
+	return 0;
+}
+catch (const std::exception &exp)
+{
+	std::cout << "Terminating due to uncaught exception:\n\texception.what(): " << exp.what();
+	return 1;
 }
